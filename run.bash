f7ee1a2c08315904bef29260efec7b3238abff1e
Francesco è volevi <deadragon99@outlook.it>
2023-06-25 15:15:24 +0200

    New metod of setup

---
+++ run.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Wait, I am installing the library..."
+pip install -q -r requirements.txt
+sudo apt install python3-pyaudio
+echo "Library installation completed!"
+echo "Running main.py..."
+python3 main.py
+echo "Press Enter to continue..."
+read
---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+
+cls
+echo Wait i am install the libray...
+pip install -q -r requirements.txt
+echo Library installation completed!
+pause
+echo Running main.py...
+python main.py
+
+
