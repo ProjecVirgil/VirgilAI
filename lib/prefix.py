c890fc9477a9ea36eb8ff47a1c77bb12903b349d
danmig06 <120124725+danmig06@users.noreply.github.com>
2023-07-27 12:31:33 +0200

    file support for prefix.py

--- lib/prefix.py
+++ lib/prefix.py
@@ -3,10 +3,17 @@ import inspect
 import time
 
 
-def Log(string:str):
+def Log(string: str, filepath: str = None):
     callstack = inspect.stack()[1]
-    caller = str(inspect.getmodule(callstack[0])).split("\\")[-1]
-    prfx=(Fore.GREEN + f"(in module {caller[:-2]}) " + time.strftime("%H:%M:%S UTC LOG", time.localtime()) + Back.RESET + Fore.WHITE)
+    caller = inspect.getmodule(callstack[0])
+    prfx = (Fore.GREEN + "(in module %S)".format(caller) + time.strftime("%H:%M:%S UTC LOG", time.localtime()) + Back.RESET + Fore.WHITE)
     prfx = (prfx + " | ")
     log = prfx + string
+    if filepath is not None:
+        try:
+            with open(filepath, "w") as f:
+                f.write(log)
+            return ''
+        except IOError:
+            return log
     return log
