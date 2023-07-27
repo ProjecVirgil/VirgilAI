7118e77114ac3bdf3065a555ba054e9e2485d031
Francesco è volevi <deadragon99@outlook.it>
2023-07-27 12:21:47 +0200

    fixed Log prefix

--- lib/prefix.py
+++ lib/prefix.py
@@ -6,7 +6,10 @@ import time
 def Log(string:str):
     callstack = inspect.stack()[1]
     caller = str(inspect.getmodule(callstack[0])).split("\\")[-1]
-    prfx=(Fore.GREEN + f"(in module {caller})" + time.strftime("%H:%M:%S UTC LOG", time.localtime()) + Back.RESET + Fore.WHITE)
+    prfx=(Fore.GREEN + f"(in module {caller[:-2]}) " + time.strftime("%H:%M:%S UTC LOG", time.localtime()) + Back.RESET + Fore.WHITE)
     prfx = (prfx + " | ")
     log = prfx + string
     return log
+
+
+print(Log("ciao"))
