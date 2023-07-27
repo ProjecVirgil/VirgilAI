69179b97ec25f1ab6a4fec4ceeeda8f2040be65d
Francesco è volevi <deadragon99@outlook.it>
2023-07-27 20:55:21 +0200

    fix some bugs

--- lib/calendarRec.py
+++ lib/calendarRec.py
@@ -3,8 +3,93 @@ import calendar
 import calendar
 import datetime
 
-from lib.prefix import Log 
-from lib.numberConvertToText import numberToWord
+#from lib.prefix import Log 
+#from lib.numberConvertToText import numberToWord
+
+def numberToWord(number: str):
+    # Definizione delle parole per i numeri da 0 a 19
+    wordsUpToVents = [
+        "zero", "uno", "due", "tre", "quattro", "cinque", "sei", "sette", "otto", "nove",
+        "dieci", "undici", "dodici", "tredici", "quattordici", "quindici", "sedici", "diciassette", "diciotto", "diciannove", "venti"
+    ]
+
+    # Definizione delle parole per le decine
+    wordDozens = [
+        "", "", "venti", "trenta", "quaranta", "cinquanta", "sessanta", "settanta", "ottanta", "novanta"
+    ]
+
+    # Definizione delle parole per le centinaia
+    wordHundreds = [
+        "", "cento", "duecento", "trecento", "quattrocento", "cinquecento", "seicento", "settecento", "ottocento", "novecento"
+    ]
+
+    def convert_under_1000(number):
+        if 0 <= number <= 999:
+            if number < 21:
+                return wordsUpToVents[number]
+            elif number < 100:
+                dozen = number // 10
+                unit = number % 10
+                # Regola di grammatica: se l'unità è 1 o 8, la decina perde l'ultima lettera
+                if unit == 1 or unit == 8:
+                    wordDozen = wordDozens[dozen][:-1]
+                else:
+                    wordDozen = wordDozens[dozen]
+                if dozen == 1 and unit == 0:
+                    return "dieci"
+                elif dozen == 8 and unit == 0:
+                    return "ottanta"
+                else:
+                    return wordDozen + wordsUpToVents[unit]
+            else:
+                hundred = number // 100
+                remainder = number % 100
+                if remainder == 0:
+                    return wordHundreds[hundred]
+                else:
+                    return wordHundreds[hundred] + convert_under_1000(remainder)
+
+    number = int(number)
+
+    if 0 <= number <= 9999:
+        if number < 1000:
+            return convert_under_1000(number)
+        else:
+            thousands = number // 1000
+            remainder = number % 1000
+            result = ""
+            if thousands == 1:
+                result += "mille"
+            else:
+                result += wordsUpToVents[thousands] + "mila"
+            if remainder > 0:
+                if remainder < 100:
+                    result += "e"
+                result += convert_under_1000(remainder)
+            return result
+
+    return "Unmanaged number"
+
+
+from colorama import Fore,Back
+import inspect
+import time
+
+
+def Log(string: str, filepath: str = None):
+    callstack = inspect.stack()[1]
+    caller = str(inspect.getmodule(callstack[0])).split("\\")[-1]
+    prfx=(Fore.GREEN + f"(in module {caller[:-2]}) " + time.strftime("%H:%M:%S UTC LOG", time.localtime()) + Back.RESET + Fore.WHITE)
+    prfx = (prfx + " | ")
+    log = prfx + string
+    if filepath is not None:
+        try:
+            with open(filepath, "w") as f:
+                f.write(log)
+            return ''
+        except IOError:
+            return log
+    return log
 
 
 
@@ -32,9 +117,9 @@ def recoverDayOfWeek(day:str,month:int = None, year:int = None):
 
 
 def recoveryDateNumber(command:str):
-    if(command in "che giorno e" or command in "che giorno e'"):
+    if(command == "che giorno e" or command == "che giorno e'"):
         day = str(datetime.datetime.now().date()).split('-')[2]
-        listOfTime = [day,None,None]
+        listOfTime = [int(day),None,None]
         return listOfTime
     if(" il " in command):   
         query=command.split("il")[1].strip()
@@ -134,6 +219,7 @@ def getDate(command:str):
     result= splitTheDate(dateNumber)
     print(result)
     return result            
+
     
         
         
