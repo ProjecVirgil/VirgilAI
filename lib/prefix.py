90906a5300fe33f2ed8055dd9f019373730f554c
danmig06 <120124725+danmig06@users.noreply.github.com>
2023-07-27 12:14:37 +0200

    Update prefix.py

--- lib/prefix.py
+++ lib/prefix.py
@@ -1,10 +1,12 @@
 from colorama import Fore,Back
+import inspect
 import time
 
 
 def Log(string:str):
-    prfx=(Fore.GREEN + time.strftime ("%H:%M:%S UTC LOG", time.localtime() )+ Back.RESET + Fore.WHITE)
+    callstack = inspect.stack()[1]
+    caller = inspect.getmodule(callstack[0])
+    prfx=(Fore.GREEN + "(in module %S)".format(caller) + time.strftime("%H:%M:%S UTC LOG", time.localtime()) + Back.RESET + Fore.WHITE)
     prfx = (prfx + " | ")
     log = prfx + string
     return log
-
