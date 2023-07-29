876b035c7bd38efabd8c8e1341e7a97d5793256a
Francesco è volevi <deadragon99@outlook.it>
2023-07-29 12:53:25 +0200

    Add more logs and i improve the variable name

--- lib/manageEvents.py
+++ lib/manageEvents.py
@@ -4,32 +4,34 @@ from lib.prefix import Log
 from lib.request import getEvents,createEvents
 
 
+# ----- Calendar Event Function -----
 
-
-def sendNotify():
-    date = datetime.datetime.now().date()
-    currentDate = date.strftime("%d-%m-%Y")
-    currentDate = currentDate.split("-")
-    currentDate[1] = currentDate[1].replace("0","")
-    currentDate  = "-".join(currentDate)
+def sendNotify():    
+    print(Log(" I take the date"),flush=True)
+    currentDate = datetime.datetime.now().date()
+    dateFormatted = currentDate.strftime("%d-%m-%Y")
+    dateFormatted = dateFormatted.split("-")
+    dateFormatted[1] = dateFormatted[1].replace("0","")
+    dateFormatted  = "-".join(dateFormatted)
+    
+    print(Log(" get the events"),flush=True)
     events = getEvents()
-    todayEvents = events[currentDate]
+    todayEvents = events[dateFormatted]
     phrase = "Ciao ti ricordo che oggi hai vari impegni: "
-    for event in todayEvents:
-        phrase = phrase + event.strip() + " "
-    print(phrase)
+    if(len(todayEvents) != 0):
+        for event in todayEvents:
+            phrase = phrase + event.strip() + " "
+    else:
+        phrase = "Oggi non hai nessun impegno goditi la giornata"
+    print(Log(f" {phrase}"),flush=True)
     return phrase
-
-
-
-
 
 #CREATE EVENT
 def splitTheDate(listOfDate:list):
     if(len(listOfDate) != 3):
         for _ in range(3-len(listOfDate)):
                 listOfDate.append(None)
-        print(Log(f" result: {listOfDate}"))  
+        print(Log(f" result: {listOfDate}"),flush=True)  
         day=listOfDate[0]
         month=listOfDate[1]
         year=listOfDate[2]
@@ -46,8 +48,7 @@ def splitTheDate(listOfDate:list):
         day=listOfDate[0]
         month=listOfDate[1]
         year=listOfDate[2]
-        print(day,month,year)
-        print(Log(" pre dayOfWeek function"))
+        print(Log(" pre dayOfWeek function"),flush=True)
         if(month != None  and year != None ):
             return(day,month,year)
         elif(year == None and month != None):
@@ -55,40 +56,39 @@ def splitTheDate(listOfDate:list):
         else:
             return(day,dateCurrente.month,dateCurrente.year)
 
-
-def getDate(divisionQuery:str):
+def getDate(command:str):
     listOfTime = []    
-    if(" il " in divisionQuery or "il " in divisionQuery):   
-        query=divisionQuery.split("il")[1].strip()
+    if(" il " in command or "il " in command):   
+        query=command.split("il")[1].strip()
     else:
         query = ""
-    division = query.split(" ")
+    divisionQuery = query.split(" ")
     
-    if(division[0].isnumeric()):
-        day=division[0]
+    if(divisionQuery[0].isnumeric()):
+        day=divisionQuery[0]
         listOfTime.append(int(day))
     else:
-        if("domani" in divisionQuery ):
-                today = datetime.datetime.today()
-                day = today + datetime.timedelta(days=1) 
+        if("domani" in command ):
+                tommorow = datetime.datetime.today()
+                day = tommorow + datetime.timedelta(days=1) 
                 listOfTime.append(day.day)
-        elif("oggi" in divisionQuery):
+        elif("oggi" in command):
                 today = datetime.datetime.today()
                 day = today + datetime.timedelta(days=0) 
                 listOfTime.append(day.day)
-        elif(( "dopo" in divisionQuery) and (divisionQuery.split(" ")[2] == "domani") ):
-                    today = datetime.datetime.today()
-                    day = today + datetime.timedelta(days=2) 
+        elif(( "dopo" in command) and (command.split(" ")[2] == "domani") ):
+                    aftertomorrow = datetime.datetime.today()
+                    day = aftertomorrow + datetime.timedelta(days=2) 
                     listOfTime.append(day.day)
                     
     months=["gennaio","febbraio","marzo","aprile","maggio","giugno","luglio","agosto","settembre","ottobre","novembre","dicembre"]
     thereAreMonth=True
     recoverMonth = ''
     for x in months:
-        if(x in division):
-            if("dopo domani" not in division):
+        if(x in divisionQuery):
+            if("dopo domani" not in divisionQuery):
                 for month in months:
-                    if(month in division[1]):
+                    if(month in divisionQuery[1]):
                         recoverMonth = months.index(month) + 1
                 listOfTime.append(recoverMonth)
                 break
@@ -97,9 +97,9 @@ def getDate(divisionQuery:str):
     if(not thereAreMonth):
         return listOfTime
     try:
-        if(division[2].isnumeric()):
-            anno = division[2]
-            listOfTime.append(int(anno))
+        if(divisionQuery[2].isnumeric()):
+            year = divisionQuery[2]
+            listOfTime.append(int(year))
         else:
             listOfTime.append(None)
     except IndexError:
@@ -107,34 +107,35 @@ def getDate(divisionQuery:str):
 
     return listOfTime         
     
-def recoverDate(command):
+def recoverDate(command:str):
     if("che" in command):
         commandSplitted = command.split(" che ")[1]
-        data = getDate(commandSplitted)
-        if(len(data) != 3 or None in data):
-            data = splitTheDate(data)
-            dataForQuery = f"{data[0]}-{data[1]}-{data[2]}"
-            return dataForQuery
+        date = getDate(commandSplitted)
+        if(len(date) != 3 or None in date):
+            date = splitTheDate(date)
+            dateFormattedCorrectly = f"{date[0]}-{date[1]}-{date[2]}"
+            return dateFormattedCorrectly
         else:
-            dataForQuery = f"{data[0]}-{data[1]}-{data[2]}"
-            return dataForQuery
+            dateFormattedCorrectly = f"{date[0]}-{date[1]}-{date[2]}"
+            return dateFormattedCorrectly
         
     elif("per" in command):
         commandSplitted= command.split(" per ")[1]
-        data = getDate(commandSplitted)
-        if(len(data) != 3 or None in data):
-            data = splitTheDate(data)
-            dataForQuery = f"{data[0]}-{data[1]}-{data[2]}"
-            return dataForQuery
+        date = getDate(commandSplitted)
+        if(len(date) != 3 or None in date):
+            date = splitTheDate(date)
+            dateFormattedCorrectly = f"{date[0]}-{date[1]}-{date[2]}"
+            return dateFormattedCorrectly
         else:
-            dataForQuery = f"{data[0]}-{data[1]}-{data[2]}"
-            return dataForQuery
+            dateFormattedCorrectly = f"{date[0]}-{date[1]}-{date[2]}"
+            return dateFormattedCorrectly
 
+# Main function
 def addEvents(command:str):
-    print(Log(" i will create event"))
+    print(Log(" i will create event"),flush=True)
     date = recoverDate(command)
-    print(Log(" i recov the date"))
+    print(Log(" i recov the date"),flush=True)
     event = command.split("ho")[1]
-    print(Log(" send the request"))
+    print(Log(" send the request"),flush=True)
     createEvents(event,date)
     return "Promemoria creato con successo"
