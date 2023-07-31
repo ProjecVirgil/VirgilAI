9ec14289cd8b007e123d84eca974b52a89820730
Retr0100 <dragonfaiver@gmail.com>
2023-07-31 13:41:12 +0200

    Add ffmpeg on a depends for make work the mediaplayer

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,6 +8,7 @@ python3 --version
 echo "Wait, I am installing the library..."
 pip install -q -r requirements.txt
 sudo apt install python3-pyaudio
+sudo apt install ffmpeg
 echo "Library installation completed!"
 echo "Press Enter to continue..."
 read
