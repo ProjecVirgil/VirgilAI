8f6607e0cf8926369bbbc03deebe8a871d0dbdae
danmig06 <120124725+danmig06@users.noreply.github.com>
2023-07-31 20:18:37 +0200

    Create logger.py

---
+++ lib/logger.py
@@ -0,0 +1,33 @@
+from colorama import Fore,Back
+
+import inspect
+import time
+
+# ---- This file make the preset for Log ----
+
+class Logger:
+    def __init__(self):
+        self.currentCallstack = ''
+        self.lastCaller = ''
+        self.__updateCallstack()
+
+    def __del__(self):
+        print(Fore.GREEN + "application terminated successfully" + Back.RESET + Fore.WHITE)
+
+    def __updateCallstack(self):
+        self.currentCallstack = inspect.stack()[1]
+        self.lastCaller = str(inspect.getmodule(self.currentCallstack[0])).split("\\")[-1]
+
+    def Log(self, string: str, filepath: str = None):
+        self.__updateCallstack()
+        prfx = (Fore.GREEN + f"(in module {self.lastCaller[:-2]}) " + time.strftime("%H:%M:%S UTC LOG", time.localtime()) + Back.RESET + Fore.WHITE)
+        prfx = (prfx + " | ")
+        log = prfx + string
+        if filepath is not None:
+            try:
+                with open(filepath, "w") as f:
+                    f.write(log)
+                return ''
+            except IOError:
+                return log
+        return log
