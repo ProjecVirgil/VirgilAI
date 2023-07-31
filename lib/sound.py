32d81c29b61725996c3393b963a041c2c671e6cb
Francesco è volevi <deadragon99@outlook.it>
2023-07-31 12:18:38 +0200

    Add new parameter for manage the case in which i run a file audio

--- lib/sound.py
+++ lib/sound.py
@@ -22,22 +22,29 @@ with open(current_path + '/setting.json') as f:
 
 
 
-def create(text:str):
+def create(text:str = "",file:bool = False,namefile:str = ""):
     pygame.mixer.music.unload()
     if(pygame.mixer.music.get_volume != float(volume)):
         pygame.mixer.music.set_volume(float(volume))
-    try:
-        sound = generate(
-            api_key = api_key,
-            text=text,
-            voice="Antoni",
-            model='eleven_multilingual_v1'
-        )
-        save(sound,'audio.mp3')    
-    except:
-        print(Log(" Google text to speech has started the cause could be a missing valid key or the end of the elevenLabs plan if you are aware of this you can ignore the message"), flush=True)
-        sound = gtts.gTTS(text,lang="it")
-        sound.save(file_path)
+        
+        if(file):
+            file = os.path.join(current_path, f"asset/{namefile}.mp3")
+            pygame.mixer.music.load(file)
+            pygame.mixer.music.play()
+            return
+        else:
+            try:
+                sound = generate(
+                    api_key = api_key,
+                    text=text,
+                    voice="Antoni",
+                    model='eleven_multilingual_v1'
+                )
+                save(sound,'audio.mp3')    
+            except:
+                print(Log(" Google text to speech has started the cause could be a missing valid key or the end of the elevenLabs plan if you are aware of this you can ignore the message"), flush=True)
+                sound = gtts.gTTS(text,lang="it")
+                sound.save(file_path)
 
     file = os.path.join(current_path, "audio.mp3")
     pygame.mixer.music.load(file)
