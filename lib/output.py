77293899f1fe4e7b4825e0424cdd2d09e6afd67b
Francesco è volevi <deadragon99@outlook.it>
2023-08-02 19:22:09 +0200

    Add the credits bunner at the shutdown function

--- lib/output.py
+++ lib/output.py
@@ -4,6 +4,8 @@ import time
 import threading
 import sys
 
+from colorama import Style,Fore
+import pyfiglet
 import pygame
 
 from lib.sound import Audio
@@ -11,7 +13,6 @@ from lib.logger import Logger
 from lib.utils  import Utils
 from lib.manageEvents import EventScheduler
 
-# ... (il resto del tuo codice)
 
 class Output:
     
@@ -20,6 +21,8 @@ class Output:
         self.logger = Logger()
         self.utils = Utils()
         self.audio = Audio()
+        pygame.init()
+
 
     def update_json_value(self,key, new_value):
         # Apri il file JSON e carica i dati
@@ -57,8 +60,6 @@ class Output:
             print(self.logger.Log(" end timer"), flush=True)
             self.audio.create(file=True, namefile="timerEndVirgil")
         
-        # Parte dell'allarme
-
     class TimerThread(threading.Thread):
         def __init__(self, interval, command):
             threading.Thread.__init__(self)
@@ -79,9 +80,16 @@ class Output:
             bool = data["0"][2]
             return res,command,bool
 
+    def shutdown(self):
+        print(self.logger.Log(" shutdown in progress..."), flush=True)
+        self.audio.create(file=True,namefile="FinishVirgil")
+        print(Style.BRIGHT +Fore.MAGENTA + pyfiglet.figlet_format("Thanks for using Virgil", font = "digital",justify= "center", width = 110 ), flush=True)
+        print(Fore.LIGHTMAGENTA_EX + " - credit: @retr0", flush=True)
+        print(Style.RESET_ALL, flush=True)
+        time.sleep(2)
+        sys.exit(0)
+
     def out(self,):
-        pygame.init()
-        #init e setup the tts
         self.audio.create(file=True,namefile="EntryVirgil")
         time.sleep(5)
         while(True):
@@ -89,19 +97,17 @@ class Output:
                 res,command,bool = self.recoverData()
                 if(res != None and bool == False):
                     if("spento" in res):
-                        print(self.logger.Log(" shutdown in progress..."), flush=True)
-                        self.audio.create(file=True,namefile="FinishVirgil")
-                    
-                        time.sleep(2)
-                        sys.exit(0)
+                        self.shutdown()
                     if("volume" in command):
+                        
                             pygame.mixer.music.set_volume(float(res))
                             pygame.mixer.music.unload()    
                             pygame.mixer.music.load('asset/bipEffectCheckSound.mp3')
                             pygame.mixer.music.play()       
                             print(self.logger.Log(f" volume changed correctly to {res*100}% "), flush=True)
-                            self.update_json_value(2, True)
+                    
                     elif("timer" in command or "sveglia" in command):
+                        
                             print(self.logger.Log(f" the timer is started see you in {res} second"), flush=True)
                             if("timer" in command):
                                 self.audio.create(f"Il timer è partito ci vediamo tra {self.utils.numberToWord(res)} secondi")
@@ -109,13 +115,13 @@ class Output:
                                 self.audio.create(f"Ho impostato la sveglia") #DA METTERRE COME PRESET
                             t = self.TimerThread(int(res),command)
                             t.start()
-                            self.update_json_value(2, True)
+                            
                     else:   
                             self.audio.create(res)
                             print(res, flush=True)
-                            self.update_json_value(2, True)
                             
-                    #Cotrollo bit
+                    self.update_json_value(2, True)
+                    
                     print(self.logger.Log(" check the reminder"),flush=True)
                     if(not self.checkReminder()):
                         print(self.logger.Log(" send notify for today event"),flush=True)
