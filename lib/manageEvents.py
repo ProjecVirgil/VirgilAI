94b4914490726acb090eeca744d9da3bd95015f2
Francesco è volevi <deadragon99@outlook.it>
2023-08-03 15:58:11 +0200

    Improve the recover date

--- lib/manageEvents.py
+++ lib/manageEvents.py
@@ -2,7 +2,7 @@ import datetime
 
 from lib.logger import Logger   
 from lib.request import MakeRequests 
-
+from lib.calendarRec import Calendar
 
 # ----- Calendar Event Function -----
 class EventScheduler:
@@ -10,6 +10,7 @@ class EventScheduler:
     def __init__(self):
         self.logger = Logger()
         self.request_maker = MakeRequests()
+        self.calendar = Calendar()
         
         print(self.logger.Log("fetching date"), flush=True)
         self.currentDate = datetime.datetime.now().date()
@@ -31,112 +32,31 @@ class EventScheduler:
         print(self.logger.Log(f" {phrase}"), flush=True)
         return phrase
 
-    #CREATE EVENT
-    def splitTheDate(self, listOfDate:list):
-        if(len(listOfDate) != 3):
-            for _ in range(3-len(listOfDate)):
-                    listOfDate.append(None)
-            print(self.logger.Log(f" result: {listOfDate}"),flush=True)
-            day = listOfDate[0]
-            month = listOfDate[1]
-            year = listOfDate[2]
-            if(month != None  and year != None ):
-                return(day,month,year)
-            elif(year == None and month != None):
-                return(day,month,self.currentDate.year)
-            else:
-                return(day,self.currentDate.month,self.currentDate.year)
+
+    def getDate(self,command):
+        presetDate  = self.calendar.recovPresetDate(command)
+        print(presetDate)
+        if(presetDate == None):
+            commandSplitted = self.calendar.splitCommand(command)
+            date = self.calendar.recovDate(commandSplitted)
+            day,month,year = date.split("-")
+            day,month = self.calendar.clearNumber(day,month)
+            date = "-".join([day,month,year])
+            return date
         else:
-            print(self.logger.Log(f" result: {listOfDate}"), flush=True)
-            day=listOfDate[0]
-            month=listOfDate[1]
-            year=listOfDate[2]
-            print(self.logger.Log(" pre dayOfWeek function"),flush=True)
-            if(month != None  and year != None ):
-                return(day,month,year)
-            elif(year == None and month != None):
-                return(day,month,self.currentDate.year)
-            else:
-                return(day,self.currentDate.month,self.currentDate.year)
+            return date
+        
 
-    def getDate(self, command:str):
-        listOfTime = []
-        if(" il " in command or "il " in command):
-            query = command.split("il")[1].strip()
-        else:
-            query = ""
-        splitQuery = query.split(" ")
-
-        if(splitQuery[0].isnumeric()):
-            day = splitQuery[0]
-            listOfTime.append(int(day))
-        else:
-            if("domani" in command ):
-                    tommorrow = self.currentDate.today()
-                    day = tommorrow + datetime.timedelta(days=1)
-                    listOfTime.append(day.day)
-            elif("oggi" in command):
-                    listOfTime.append(self.currentDate.day)
-            elif(( "dopo" in command) and (command.split(" ")[2] == "domani") ):
-                    aftertomorrow = self.currentDate.today()
-                    day = aftertomorrow + datetime.timedelta(days=2)
-                    listOfTime.append(day.day)
-
-        months=["gennaio","febbraio","marzo","aprile","maggio","giugno","luglio","agosto","settembre","ottobre","novembre","dicembre"]
-        thereAreMonth=True
-        recoverMonth = ''
-        for x in months:
-            if(x in splitQuery):
-                if("dopo domani" not in splitQuery):
-                    for month in months:
-                        if(month in splitQuery[1]):
-                            recoverMonth = months.index(month) + 1
-                    listOfTime.append(recoverMonth)
-                    break
-        else:
-            thereAreMonth=False
-        if(not thereAreMonth):
-            return listOfTime
-        try:
-            if(splitQuery[2].isnumeric()):
-                year = splitQuery[2]
-                listOfTime.append(int(year))
-            else:
-                listOfTime.append(None)
-        except IndexError:
-            listOfTime.append(None)
-
-        return listOfTime
-
-    def recoverDate(self, command:str):
+    def addEvents(self,command:str):
+        print(self.logger.Log(" i will create event"),flush=True)
         if("che" in command):
             commandSplit = command.split(" che ")[1]
             date = self.getDate(commandSplit)
-            if(len(date) != 3 or None in date):
-                date = self.splitTheDate(date)
-                dateFormattedCorrectly = f"{date[0]}-{date[1]}-{date[2]}"
-                return dateFormattedCorrectly
-            else:
-                dateFormattedCorrectly = f"{date[0]}-{date[1]}-{date[2]}"
-                return dateFormattedCorrectly
-
         elif("per" in command):
-            commandSplit= command.split(" per ")[1]
+            commandSplit = command.split(" per ")[1]
             date = self.getDate(commandSplit)
-            if(len(date) != 3 or None in date):
-                date = self.splitTheDate(date)
-                dateFormattedCorrectly = f"{date[0]}-{date[1]}-{date[2]}"
-                return dateFormattedCorrectly
-            else:
-                dateFormattedCorrectly = f"{date[0]}-{date[1]}-{date[2]}"
-                return dateFormattedCorrectly
-
-# Main function
-def addEvents(self,command:str):
-    print(self.logger.Log(" i will create event"),flush=True)
-    date = self.recoverDate(command)
-    print(self.logger.Log(" i recov the date"),flush=True)
-    event = command.split("ho")[1]
-    print(self.logger.Log(" send the request"),flush=True)
-    MakeRequests.createEvents(event,date)
-    return "Promemoria creato con successo"
+        print(self.logger.Log(" i recov the date"),flush=True)
+        event = command.split("ho")[1]
+        print(self.logger.Log(" send the request"),flush=True)
+        MakeRequests.createEvents(event,date)
+        return "Promemoria creato con successo"
