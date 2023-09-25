e31aa88617403a6ad65804586644242e46c476b8
Francesco è volevi <deadragon99@outlook.it>
2023-09-25 18:21:48 +0200

    Clean and improve the code

--- setup/setup.py
+++ setup/setup.py
@@ -8,36 +8,29 @@ import colorama
 import tomli
 from tomlkit import parse, dumps
 import pyfiglet
-
-#TODO(Retr0) MAKE THE LINUX VERSION
-#* -  ADD the command for enable and remove the function to startup
-
-# - crontab -e (Si apre un testo)
-# - aggiungere questa riga
-# - @reboot python /bin/test_code.py & (vedere se specificare o no & quindi a seconda delle impostazioni fare di conseguenza)
-# - PER TOGLIERE STESSA COSA MA BISOGNA RIMUOVERE LA LINEA
-# - NEL FILE DI LAUNCH GESTIRE LA QUESTIONE NASCOSTO SIA PER LINUX CHE PER WIN
-#
-
-
-
-
 # ------ WINDOWS -------
 
-def windows_function(path_directory,display=True):
+def windows_function(path_directory):
     """Function to with command for Windows."""
-    if display:
-        with open("launch_start.bat","w")as file:
-            file.write(f"python {path_directory}\\launch.py")
-    else:
-        with open("launch_start.bat","w")as file:
-            file.write(f"python {path_directory}\\launch.pyw")
-
+    print(path_directory)
+    with open("launch_start.bat","w")as file:
+        file.write(f"python {path_directory}\\launch.py")
     source_path = f"{path_directory}\\setup\\launch_start.bat"
+    print(source_path)
     current_user = os.getlogin()
     source_path = 'F:\\ProjectVirgil\\VirgilAI\\setup\\launch_start.bat'
     destination_folder = f'C:\\Users\\{current_user}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'
-    shutil.copy2(source_path, destination_folder)
+    print(source_path)
+    print(destination_folder)
+    try:
+        shutil.copy2(source_path, destination_folder)
+    except OSError:
+        print(colorama.Fore.RED+ colorama.Style.BRIGHT + '''
+ Oops your antivirus doesn't like Virgilio to start automatically please
+ give the green light to the antivirus to run the bat file (If you have
+ any doubts about security I invite you to analyze the whole source code
+ and check its security and if you still don't trust it you can simply
+ don't run it at startup)''')
     os.remove(source_path)
 
 def modify_start_startup_win(launch_start):
@@ -63,12 +56,7 @@ def modify_start_startup_win(launch_start):
 
 def modify_display():
     """This function is used for modifying the configuration of the display."""
-    toml_path = '../pyproject.toml'
-    with open(toml_path, "rb") as file:
-        metadata = tomli.load(file)
-        defaul_start = metadata["tool"]["config_system"]["defaul_start"]
-        display_console = metadata["tool"]["config_system"]["display_console"]
-
+    _,_,defaul_start,display_console = get_data()
     path = get_path()
     if(defaul_start in ('T','N') and display_console is not True):
             if(platform.system() == "Windows"):
@@ -80,7 +68,7 @@ def modify_display():
                 linux_function(path,display=True)
                 print(colorama.Fore.GREEN + colorama.Style.BRIGHT +"\n ***** Successfully executed changes *****")
     elif(defaul_start in ('T','N') and display_console is True):
-            print(colorama.Fore.RED + colorama.Style.BRIGHT +"\n You can't set the display to False with default startup to Text \n if you can set to display true first set the interface to text or remove the dafault ") #TODO TEST
+            print(colorama.Fore.RED + colorama.Style.BRIGHT +"\n You can't set the display to False with default startup to Text \n if you can set to display true first set the interface to text or remove the dafault ")
     else:
         if platform.system() == "Windows":
             os.rename("../launch.py","../launch.pyw")
@@ -104,37 +92,22 @@ def check_system() -> str:
         return "L"
     return "N"
 
+def get_data():
+    """Get metadata from pyproject.toml.
 
+    Returns:
+        tupla: All the metadata neccesary for the setup
+    """
+    toml_path = '../pyproject.toml'
+    with open(toml_path, "rb") as file:
+        metadata = tomli.load(file)
+        first_setup = metadata["tool"]["config_system"]["first_setup"]
+        launch_start = metadata["tool"]["config_system"]["launch_start"]
+        defaul_start = metadata["tool"]["config_system"]["defaul_start"]
+        display_console = metadata["tool"]["config_system"]["display_console"]
+        file.close()
 
-def windows_function(path_directory):
-    """Function to with command for Windows."""
-    print(path_directory)
-    with open("launch_start.bat","w")as file:
-        file.write(f"python {path_directory}\\launch.py")
-    source_path = f"{path_directory}\\setup\\launch_start.bat"
-    print(source_path)
-    current_user = os.getlogin()
-    source_path = 'F:\\ProjectVirgil\\VirgilAI\\setup\\launch_start.bat'
-    destination_folder = f'C:\\Users\\{current_user}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'
-    print(source_path)
-    print(destination_folder)
-    try:
-        shutil.copy2(source_path, destination_folder)
-    except OSError:
-        print(colorama.Fore.RED+ colorama.Style.BRIGHT + '''
- Oops your antivirus doesn't like Virgilio to start automatically please
- give the green light to the antivirus to run the bat file (If you have
- any doubts about security I invite you to analyze the whole source code
- and check its security and if you still don't trust it you can simply
- don't run it at startup)''')
-    os.remove(source_path)
-
-
-
-def linux_function(command, path_directory=None,):
-    """Function to with command for Linux."""
-    print("Workin in progess")
-
+    return first_setup,launch_start,defaul_start,display_console
 
 def update_toml(params: str, new_value: str):
     """Update TOML file from params dictonary.
@@ -154,38 +127,14 @@ def update_toml(params: str, new_value: str):
 
 def show_settings():
     """This function shows all settings."""
-    toml_path = '../pyproject.toml'
-    with open(toml_path, "rb") as file:
-        metadata = tomli.load(file)
-        launch_start = metadata["tool"]["config_system"]["launch_start"]
-        defaul_start = metadata["tool"]["config_system"]["defaul_start"]
-        display_console = metadata["tool"]["config_system"]["display_console"]
+    _,launch_start,defaul_start,display_console = get_data()
     mss_list = ["The option for the launch start with the system is: ","The option for the default launch is: ","The option for the show of display is: ",]
     value = [launch_start,defaul_start,display_console]
     print(colorama.Fore.BLUE + colorama.Style.BRIGHT + "\n\t    ------ [SETTINGS] -------\n")
     for mss in mss_list:
         print(colorama.Fore.CYAN + colorama.Style.BRIGHT + " - " + mss + colorama.Fore.GREEN + str(value[mss_list.index(mss)]))
 
-# ----- LINUX -------
-
-
-def linux_function(path_directory=None,display=True):
-    """Function to with command for Linux."""
-    # PER LINUX GENERARE SOLO IL COMANDO E FARLO ESEGUIRE ALL UTENTE
-    # Comando per aggiungere un lavoro Crontab che si esegue all'avvio
-    if display:
-        job = f"@reboot screen -dmS virgil /usr/bin/python3 {path_directory}/launch.py"
-        print("\n" + colorama.Fore.RED +colorama.Style.BRIGHT +"For connect to the terminal execute this command: screen -r virgil")
-    else:
-        job = f"@reboot /usr/bin/python3 {path_directory}/launch.py"
-
-    command = f"echo {job} | crontab -"  # Comando completo
-    try:
-        subprocess.run(command, shell=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error on Crontab: {e}")
-
-
+# ----- LINUX ------
 def modify_start_startup_lin(launch_start,display):
     """Modify the start of Virgil on startup.
 
@@ -203,6 +152,22 @@ def modify_start_startup_lin(launch_start,display):
     else:
         linux_function(get_path,display=display)
         update_toml("launch_start",not launch_start)
+
+def linux_function(path_directory=None,display=True):
+    """Function to with command for Linux."""
+    # PER LINUX GENERARE SOLO IL COMANDO E FARLO ESEGUIRE ALL UTENTE
+    # Comando per aggiungere un lavoro Crontab che si esegue all'avvio
+    if display:
+        job = f"@reboot screen -dmS virgil /usr/bin/python3 {path_directory}/launch.py"
+        print("\n" + colorama.Fore.RED +colorama.Style.BRIGHT +"For connect to the terminal execute this command: screen -r virgil")
+    else:
+        job = f"@reboot /usr/bin/python3 {path_directory}/launch.py"
+
+    command = f"echo {job} | crontab -"  # Comando completo
+    try:
+        subprocess.run(command, shell=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error on Crontab: {e}")
 
 # ---- FIRST START ------
 
@@ -285,7 +250,6 @@ def first_start(cli,parent_folder):
     update_toml("first_setup",False)
 
 
-
 def setup(launch_start,defaul_start,display_console):
     """This function is used for setting up all of the things that are needed before running the programm. It will create a config file and also check whether.
 
@@ -349,26 +313,24 @@ colorama.Fore.BLUE +'''
             sys.stdout.flush()
 
 def get_path():
+    """Get path to current dir.
+
+    Returns:
+        str: Path
+    """
     current_folder = os.getcwd()
     parent_folder = os.path.dirname(current_folder)
     return parent_folder
 
 def main():
     """Main function."""
-
     subprocess.run("poetry install",shell=True)
 
     print(colorama.Style.BRIGHT + colorama.Fore.MAGENTA +
           pyfiglet.figlet_format("  Virgil", font="doh", width=200), flush=True)
 
     # ------ SETUP -------
-    toml_path = '../pyproject.toml'
-    with open(toml_path, "rb") as file:
-        metadata = tomli.load(file)
-        first_setup = metadata["tool"]["config_system"]["first_setup"]
-        launch_start = metadata["tool"]["config_system"]["launch_start"]
-        defaul_start = metadata["tool"]["config_system"]["defaul_start"]
-        display_console = metadata["tool"]["config_system"]["display_console"]
+    first_setup,launch_start,defaul_start,display_console = get_data()
 
     parent_folder = get_path()
 
@@ -390,8 +352,11 @@ def main():
     # ------- END SETUP -----
 
     if(first_setup):
+
         if(system == "L"):
+            #DOWNLOAD OFF SOME PACKET
             subprocess.run("sudo apt install python3-pyaudio;sudo apt install ffmpeg;sudo apt install screen",shell=True)
+
         first_start(cli,parent_folder)
     else:
         setup(launch_start,defaul_start,display_console)
