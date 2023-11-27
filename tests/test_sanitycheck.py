08ac76c18c719b71f985b956d1f4ecfb8be593de
Retr0 <dragonfaiver@gmail.com>
2023-11-27 20:02:06 +0100

    Refactor tests to include API model calls
    This change adds two new test functions that make API calls to test the
    functionality of the API models. The tests verify the correct prediction
    of classes based on given inputs. The inclusion of these tests enhances
    the test coverage and ensures the accuracy of the API models. No issues
    are associated with this change.

--- tests/test_sanitycheck.py
+++ tests/test_sanitycheck.py
@@ -1,7 +1,21 @@
 """Test file."""
 
+import requests
+
 def test_default():
     """Function test."""
     foo = 1
     bar = 1
     assert foo == bar
+
+BASE_URL = "http://127.0.0.1:8080"
+
+def test_call_api_models_ita():
+    """Function test api-models."""
+    r = requests.get(BASE_URL + "/q/it/Che tempo fa domani a Salerno")
+    assert {"Classes predicted":"MT"} == r.json()
+
+def test_call_api_models_en():
+    """Function test api-models."""
+    r = requests.get(BASE_URL + "/q/en/The weather tomorrow in Salerno")
+    assert {"Classes predicted":"MT"} == r.json()
