643693593d80b275d85c0824f8a2bd3f92e9f6c5
Retr0 <dragonfaiver@gmail.com>
2024-01-01 20:04:18 +0100

    feat: Add CommandProcessor class
    This commit adds the CommandProcessor class, which is responsible for managing all the commands and selecting the appropriate function to execute. It also includes functions for loading the language model, cleaning the command, and sending the command for processing.

---
+++ lib/packages_main/command_processor.py
@@ -0,0 +1,143 @@
+"""This module is much important because manage all the class and process the command."""
+import queue
+import string
+import sys
+import time
+
+from pygame import mixer
+import joblib
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+from lib.packages_utility.logger import logging
+from lib.packages_utility.command_selector import CommandSelector
+
+
+# ---- File for manage all the preset command ----
+
+
+
+class CommandProcessor:
+    """This class is used to select a specific function of the assistant."""
+
+    def __init__(self, settings,result_queue:queue.Queue,class_manager) -> None:
+        """Init all the settings for manage all classes.
+
+        Args:
+            settings (Settings): the dataclasses with all settings
+            result_queue (Queue): The queue with results
+            class_manager (ClassManager): This dataclasses manage some classes like utils and audio
+        """
+        self.settings = settings
+        lang = settings.language
+
+        self.audio = class_manager.audio
+
+        self.command_selector  = CommandSelector(settings,class_manager)
+
+        self.result_queue = result_queue
+
+        self.loaded_model = self.load_model(lang)
+
+        original_punctuation = string.punctuation
+        exceptions = ":'"
+        self.custom_punctuation = "".join([char for char in original_punctuation if char not in exceptions])
+
+    def off(self) -> None:
+        """Function to shutdown all services and close connection with database."""
+        print("\nVirgil: Shutdown in progress...", flush=True)
+        data = ["shutdown", "shutdown"]
+        self.result_queue.put(data)
+        time.sleep(2)
+        sys.exit(0)
+
+    def load_model(self,language):
+        """Load language model from file.
+
+        Args:
+            language (_type_): _description_
+        """
+        model_filename = f"model/model_{language}.pkl"
+        loaded_model = joblib.load(model_filename)
+        if language == 'it':
+            self.stop_words = set(stopwords.words("italian"))
+        else:
+            self.stop_words = set(stopwords.words("english"))
+        return loaded_model
+
+    def clean(self, command: str, type_model: str) -> str | list[str]:
+        """Function that cleans a command.
+
+        Args:
+            command (str): the command to clean
+            type_model (str): type of cleaning model(cleaning the command for
+            work with the models) work (cleaning the command for work with the my code)
+
+        Returns:
+            str: The command cleaned
+        """
+        try:
+            if type_model == 'model':
+                command = command.lower()
+                command = command.translate(str.maketrans(' ', ' ', self.custom_punctuation))
+                words = command.split()
+                return ' '.join(words)
+            if type_model == 'work':
+                filtered_tokens = []
+                tokens = word_tokenize(command.lower().strip())
+                for word in tokens:
+                    if word not in (",", "?") and word not in self.stop_words:
+                        filtered_tokens.append(word)
+                logging.debug(f" command processed: {filtered_tokens} ")
+                return filtered_tokens
+        except IndexError:
+            return command
+
+    def send_command(self, command:str) -> str | None:  # noqa: PLR0911, PLR0912, PLR0915
+        """Function to process a command received by user and send on process.
+
+        Args:
+            command (str): command cleaned
+
+        Returns:
+            str: The response at the command
+        """
+        mixer.init()
+        predict = self.loaded_model.predict([self.clean(command, "model")])[0]
+        command_worked = self.clean(command, "work")
+        logging.info(f"Classes chosen by algorithm is {predict}")
+
+        table_function = {
+            'OR': self.command_selector.get_time,
+            'VL': self.command_selector.change_volume,
+            'MT': self.command_selector.get_weather,
+            'TM': self.command_selector.get_timer,
+            'GDS':self.command_selector.get_date,
+            'MC': self.command_selector.get_mc,
+            'NW': self.command_selector.get_news,
+            'MU': self.command_selector.play_music,
+            'EV': self.command_selector.add_events,
+            'AL': self.command_selector.generate_response
+        }
+
+        try:
+            # * --- SPECIAL COMMAND -----
+            if (self.settings.split_command[0] in command_worked) or (self.settings.split_command[1] in command_worked):
+                self.off()
+                return
+            elif (self.settings.split_command[6] in command_worked) or (self.settings.split_command[7] in command_worked) or (self.settings.split_command[8] in command_worked):
+                mixer.music.stop()
+                return
+
+            # * --- STANDARD COMMAND -----
+            elif(predict != 'AL'):
+                logging.info("Command understood correctly")
+                return table_function[predict](command_worked)
+            else:
+                logging.info("Let me think I am generating the answer...")
+                return table_function[predict](command)
+        except Exception as error: # If an error appears
+                    logging.error("Please try the command again")
+                    logging.error(f"ERROR: {error}")
+                    self.audio.create(file = True,namefile="ErrorCommand")
+                    return
